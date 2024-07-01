9bca2a1235fd760c1133a49733de69e62a99f70d
Victor <victorscoderepo@gmail.com>
2024-07-01 12:58:52 -0400

    Morse code timings shortened

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -29,21 +29,21 @@ public class Main {
         for (int i = 0; i < OutputArrayLength; i ++) {
             if (OutputArray[i] == '.') {
                 led.high();
-                Thread.sleep(1000);
+                Thread.sleep(500);
                 led.low();
             }
             if (OutputArray[i] == '-') {
                 led.high();
-                Thread.sleep(3000);
+                Thread.sleep(1500);
                 led.low();
             }
             if (OutputArray[i] == '/') {
-                Thread.sleep(2000);
+                Thread.sleep(1000);
             }
             if (OutputArray[i] == ' ') {
-                Thread.sleep(6000);
+                Thread.sleep(3000);
             }
-            Thread.sleep(1000);
+            Thread.sleep(500);
         }
         pi4j.shutdown();
     }
